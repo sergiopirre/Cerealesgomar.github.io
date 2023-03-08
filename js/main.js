0a95ecdfd62834f19e79e208acd048cc0453a9bb
sergiopirre <117481229+sergiopirre@users.noreply.github.com>
2023-03-08 20:55:18 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-
+document.addEventListener ('DOMContentLoaded', () => {
+    const elementoscarrousel = document.querySelectorAll('.carrousel');
+    M.carrousel.init(elementoscarrousel, {
+        duration: 150
+    });
+});
