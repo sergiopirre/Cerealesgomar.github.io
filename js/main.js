96a5ca07fb692f44e08da4fd18d1e01b4526b473
sergiopirre <117481229+sergiopirre@users.noreply.github.com>
2023-03-13 21:02:52 +0100

    Delete js directory

--- js/main.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener ('DOMContentLoaded', () => {
-    const elementoscarrousel = document.querySelectorAll('.carrousel');
-    M.carrousel.init(elementoscarrousel, {
-        duration: 150
-    });
-});
